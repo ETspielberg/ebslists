a230083b5d1edc09ba74cc8bac5adbbd917364d4
Eike Spielberg <Eike.Spielberg@uni-due.de>
2018-09-10 11:56:56 +0200

    working version with different modes of analysis

---
+++ README.md
@@ -0,0 +1,69 @@
+#EBS-Analyzer
+The EBS-Anaylzer analyzes usage data provided by the publisher.
+The im is to provide a preselection of titles for further licencing based on the usage and prices of the individual titles.
+
+It is written as a python3 application using the flask framework.
+
+
+
+##Setup
+### Clone project
+```
+$ git clone https://github.com/ETspielberg/ebslists
+```
+
+
+### Install requeirements
+The programme is written as python flask application. 
+Necessary dependencies are listed in the requirements.txt and can be installed via
+```
+$ pip install -r requirements.txt
+```
+
+### Configuration
+The configuration needs only the folder where the csv files with the usage data are stored.
+To implement it into a microservice framework it can be configured to the upload directory of the system.
+In any case, a subdirectory `/ebslists` is appended.
+```
+LIBINTEL_UPLOAD_DIR = "${USER_HOME}/.libintel/uploads"
+```
+
+##file format
+The file containing the usage data needs to have the following columns.
+```
+ISBN; title; subject area; year; total usage; price; ...
+```
+
+### Start-Up
+To start the application the virtual environment has to be activated.
+After that some environmental parameters need to be set for the flask application.
+For a development version the following code can be executed: 
+```
+./venv/Scripts/activate
+$env:FLASK_APP="start.py"
+$env:FLASK_ENV="development"
+$env:LIBINTEL_SETTINGS = "${USER_HOME}\.libintel\config\ebslists.cfg"
+python -m flask run
+```
+
+File is saved in the upload directory specified in the config file.
+
+##Running an analysis
+After the file has been uploaded (or is stored in the folder `${USER_HOME}/.libintel/uploads/ebslists/`)
+an analysis is started using a HTTP POST request, for example using cURL:
+```
+curl -X POST -F 'filename='<filename> -F 'model=<model>' -F 'limit=<limit>' \
+    -F 'mode=<mode>' http://<server.address>:<server.port>/ebslists
+```
+`<filename>` corresponds to the filename of the usage data, `<model>` is an identifier for the ebs model (e.g. ebs_publisher_year),
+ `<limit>` is the price limit for the ebs model, `<mode>` selects one the following modes of analysis:
+ 
+* `only_usage`: Ranking anhand der Nutzung
+* `only_cost_per_usage`: Ranking anhand Preis pro Nutzung
+* `price_normalized_percentiles`: Ranking sowohl anhand von Preis und Preis pro Nutzung. Die beiden Teillisten werden auf den gleichen Wert normalisiert.
+* `percentage_normalized_percentiles`: Ranking sowohl anhand von Preis und Preis pro Nutzung. Die beiden Teillisten werden auf die gleiche Anzahl von Einträgen normalisiert.
+* `usage_normalized_percentile`: Ranking sowohl anhand von Preis und Preis pro Nutzung. Die beiden Teillisten werden auf die gleiche Nutzung normalisiert.
+* `index`: Summer der Indizes der Ranking Listen als Reihungsfaktor
+* `value_weighting`: Produkt derr Wichtungsfaktoren
+* `value_weighting_exponential`: Produkt der exponentiellen Wichtungsfaktoren
+
--- start.py
+++ start.py
@@ -1,9 +1,11 @@
 import csv
 import json
 import requests
+import math
 
 from flask import Flask
 from flask import request
+from statistics import mean
 
 from model.EbsTitle import EbsTitle
 
@@ -14,106 +16,29 @@ app.config.from_envvar("LIBINTEL_SETTINGS")
 location = app.config.get("LIBINTEL_UPLOAD_DIR") + "\\ebslists\\"
 
 
-def set_bools(virtual_limit, ebs_titles, ebs_model):
-    total_sum = 0
-    selected_sum = 0
-    if (ebs_model == 'price_normalized_percentiles'):
-        ebs_titles.sort(key=lambda x: x.totalUsage, reverse=True)
-        for title in ebs_titles:
-            total_sum += title.price
-            title.selection_usage = (total_sum < virtual_limit)
-        ebs_titles.sort(key=lambda x: x.pricePerUsage, reverse=False)
-        total_sum = 0
-        for title in ebs_titles:
-            total_sum += title.price
-            title.selection_price_per_usage = (total_sum < virtual_limit)
-            title.selected = title.selection_price_per_usage and title.selection_usage
-            if title.selected:
-                selected_sum += title.price
-                print(title.title)
-            if title.price == 0:
-                title.selected = True
-    elif (ebs_model == 'normalized_percentiles'):
-        ebs_titles.sort(key=lambda x: x.totalUsage, reverse=True)
-        for title in ebs_titles:
-            total_sum += title.price
-            title.selection_usage = (total_sum < virtual_limit)
-        ebs_titles.sort(key=lambda x: x.pricePerUsage, reverse=False)
-        total_sum = 0
-        for title in ebs_titles:
-            total_sum += title.price
-            title.selection_price_per_usage = (total_sum < virtual_limit)
-            title.selected = title.selection_price_per_usage and title.selection_usage
-            if title.selected:
-                selected_sum += title.price
-                print(title.title)
-            if title.price == 0:
-                title.selected = True
-    elif (ebs_model == 'percentiles') :
-        ebs_titles.sort(key=lambda x: x.totalUsage, reverse=True)
-        for title in ebs_titles:
-            total_sum += title.price
-            title.selection_usage = (total_sum < virtual_limit)
-        ebs_titles.sort(key=lambda x: x.pricePerUsage, reverse=False)
-        total_sum = 0
-        for title in ebs_titles:
-            total_sum += title.price
-            title.selection_price_per_usage = (total_sum < virtual_limit)
-            title.selected = title.selection_price_per_usage and title.selection_usage
-            if title.selected:
-                selected_sum += title.price
-                print(title.title)
-            if title.price == 0:
-                title.selected = True
-    elif (ebs_model == 'only_usage'):
-        ebs_titles.sort(key=lambda x: x.totalUsage, reverse=True)
-        for title in ebs_titles:
-            total_sum += title.price
-            title.selection_usage = (total_sum < virtual_limit)
-            title.selected = title.selection_usage;
-            selected_sum += title.price
-    elif (ebs_model == 'only_cost_per_usage'):
-        ebs_titles.sort(key=lambda x: x.pricePerUsage, reverse=False)
-        for title in ebs_titles:
-            total_sum += title.price
-            title.selection_price_per_usage = (total_sum < virtual_limit)
-            title.selected = title.selection_price_per_usage
-            selected_sum += title.price
-    return selected_sum
-
-
 @app.route('/ebslists', methods=['POST'])
 def ebslist():
-    filename = request.form['filename']
-    ebs_model = request.form['ebsModel']
-    limit = float(request.form['limit'])
-    virtual_limit = 1.5 * limit
-    ebs_titles = load_data(filename)
-    old_selected_sum = 0
-    selected_sum = set_bools(virtual_limit, ebs_titles)
-    step_differ = True
-    n_cycles = 0
-    while step_differ:
-        n_cycles +=1
-        difference = selected_sum - limit
-        virtual_limit -= difference * 0.8
-        old_selected_sum = selected_sum
-        selected_sum = set_bools(virtual_limit, ebs_titles, ebs_model)
-        if (selected_sum - old_selected_sum) == 0:
-            step_differ = False
-        if n_cycles == 100:
-            step_differ = False
-    with open(location + filename.replace(".csv", "") + "_out.csv", 'w', newline='') as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=';',
-                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        spamwriter.writerow(['ISBN', 'Title', 'Subject area', 'price', 'year', 'total usage', 'price per usage', 'selected'])
-        for item in ebs_titles:
-            spamwriter.writerow([item.isbn, item.title, item.subjectArea, str(item.price), str(item.year), str(item.totalUsage), str(item.pricePerUsage), str(item.selected), ebs_model])
-    # save_ebs_list(ebs_list)
+    # reading parameters from HTTP-request
+    ebs_filename = request.form['filename']
+    ebs_model = request.form['model']
+    ebs_mode = request.form['mode']
+    ebs_limit = float(request.form['limit'])
+
+    # load the data from the data file in the upload directory
+    ebs_titles = load_data(ebs_filename, ebs_model)
+
+    # make the selection, i.e. set the boolean "selectected" and weighting factors if necessary
+    selected_sum = make_selection(ebs_limit, ebs_titles, ebs_mode)
+
+    # persist the results to the database in order to offer them to the fachref-assistant
+    # persist_ebs_list(ebs_list)
+
+    # save the results to a _out-file in the upload directory
+    save_ebs_list_file(ebs_titles, ebs_filename, ebs_model, ebs_mode)
     return str(selected_sum) + ' selected'
 
 
-def load_data(filename):
+def load_data(filename, ebs_model):
     with open(location + filename, 'r') as csvfile:
         linereader = csv.reader(csvfile, delimiter=';')
         ebs_titles = []
@@ -121,26 +46,297 @@ def load_data(filename):
             try:
                 isbn = row[0]
                 title = row[1]
+                if ";" in title:
+                    title.replace(";", ".")
                 subject_area = row[2]
-                price = float(row[5])
+                price_string = row[5]
+                if "." in price_string:
+                    price_string = price_string.replace(".", "")
+                try:
+                    price = float(price_string)
+                except ValueError:
+                    price = 0
                 try:
                     year = int(row[3][-4:])
                 except ValueError:
                     year = 0
-                total_usage = int(row[4])
+                total_usage_string = row[4]
+                if "." in total_usage_string:
+                    total_usage_string = total_usage_string.replace(".", "")
+                total_usage = int(total_usage_string)
                 if total_usage != 0:
-                    price_per_usage = price / total_usage
+                    cost_per_usage = price / total_usage
                 else:
-                    price_per_usage = price
-                ebs_title = EbsTitle(isbn, title, subject_area, price, year, total_usage, price_per_usage, False, False,
-                                     False, ebs_model)
+                    cost_per_usage = price
+                ebs_title = EbsTitle(isbn, title, subject_area, price, year, total_usage, cost_per_usage, True, True,
+                                     False, ebs_model, 1)
                 ebs_titles.append(ebs_title)
             except ValueError:
                 print('no values')
+                print(row)
+    return ebs_titles
 
-def save_ebs_list(ebs_titles):
+
+def make_selection(ebs_limit, ebs_titles, ebs_mode):
+    if 'only_usage' in ebs_mode:
+        set_bools_usage_for_cost_limit(ebs_titles, ebs_limit)
+        return get_price_for_selection(ebs_titles)
+    elif 'only_cost_per_usage' in ebs_mode:
+        set_bools_cost_per_usage_for_cost_limit(ebs_titles, ebs_limit)
+        return get_price_for_selection(ebs_titles)
+    elif 'price_normalized_percentiles' in ebs_mode:
+        virtual_limit = 1.5 * ebs_limit
+        set_bools_usage_for_cost_limit(ebs_titles, virtual_limit)
+        set_bools_cost_per_usage_for_cost_limit(ebs_titles, virtual_limit)
+        price_selected = get_price_for_selection(ebs_titles)
+        step_differ = True
+        n_cycles = 0
+        while step_differ:
+            n_cycles += 1
+            difference = price_selected - ebs_limit
+            virtual_limit -= difference * 0.8
+            old_selected_sum = price_selected
+            set_bools_usage_for_cost_limit(ebs_titles, virtual_limit)
+            set_bools_cost_per_usage_for_cost_limit(ebs_titles, virtual_limit)
+            price_selected = get_price_for_selection(ebs_titles)
+            if (price_selected - old_selected_sum) == 0:
+                step_differ = False
+            if n_cycles == 100:
+                step_differ = False
+    elif 'percentage_normalized_percentiles' in ebs_mode:
+        mean_price = mean(title.price for title in ebs_titles)
+        number = ebs_limit // mean_price
+        make_selection_for_usage_with_threshold(ebs_titles, number)
+        make_selection_for_cost_per_usage_with_threshold(ebs_titles, number)
+        price_selected = get_price_for_selection(ebs_titles)
+        step_differ = True
+        n_cycles = 0
+        while step_differ:
+            n_cycles += 1
+            old_selected_sum = price_selected
+            if price_selected != 0:
+                fraction = float((price_selected - ebs_limit)) / float(price_selected)
+            else:
+                fraction = 2
+            number = number * (1 - fraction)
+            make_selection_for_usage_with_threshold(ebs_titles, int(number))
+            make_selection_for_cost_per_usage_with_threshold(ebs_titles, int(number))
+            price_selected = get_price_for_selection(ebs_titles)
+            print(price_selected)
+            if (price_selected - old_selected_sum) == 0:
+                step_differ = False
+            if n_cycles == 100:
+                step_differ = False
+    elif 'usage_normalized_percentiles' in ebs_mode:
+        mean_usage = mean(title.total_usage for title in ebs_titles)
+        mean_price = mean(title.price for title in ebs_titles)
+        usage_threshold = int(ebs_limit/mean_price * mean_usage)
+        set_bools_usage_for_usage_limit(ebs_titles, usage_threshold)
+        set_bools_cost_per_usage_for_usage_limit(ebs_titles, usage_threshold)
+        price_selected = get_price_for_selection(ebs_titles)
+        print(price_selected)
+        step_differ = True
+        n_cycles = 0
+        while step_differ:
+            n_cycles += 1
+            old_selected_sum = price_selected
+            if price_selected != 0:
+                fraction = float((price_selected - ebs_limit)) / (8* ebs_limit)
+            else:
+                fraction = 2
+            usage_threshold = usage_threshold * (1 - fraction)
+            set_bools_usage_for_usage_limit(ebs_titles, usage_threshold)
+            set_bools_cost_per_usage_for_usage_limit(ebs_titles, usage_threshold)
+            price_selected = get_price_for_selection(ebs_titles)
+            print(price_selected)
+            if (price_selected - old_selected_sum) == 0:
+                step_differ = False
+            if n_cycles == 100:
+                step_differ = False
+    elif 'index' == ebs_mode:
+        set_position_for_usage(ebs_titles)
+        set_position_for_cost_per_usage(ebs_titles)
+        price_selected = get_price_for_list_with_factor(ebs_titles, ebs_limit)
+    elif 'index_weighting' == ebs_mode:
+        set_weighting_for_position_usage(ebs_titles)
+        set_weighting_for_position_cost_per_usage(ebs_titles)
+        price_selected = get_price_for_list_with_weighting(ebs_titles, ebs_limit)
+    elif 'value_weighting' == ebs_mode:
+        set_weighting_for_usage(ebs_titles)
+        set_weighting_for_cost_per_usage(ebs_titles)
+        price_selected = get_price_for_list_with_weighting(ebs_titles, ebs_limit)
+    elif 'index_weighting_exponential' == ebs_mode:
+        set_exponential_weighting_for_position_usage(ebs_titles)
+        set_exponential_weighting_for_position_cost_per_usage(ebs_titles)
+        price_selected = get_price_for_list_with_weighting(ebs_titles, ebs_limit)
+    elif 'value_weighting_exponential' == ebs_mode:
+        set_exponential_weighting_for_usage(ebs_titles)
+        set_exponential_weighting_for_cost_per_usage(ebs_titles)
+        price_selected = get_price_for_list_with_weighting(ebs_titles, ebs_limit)
+    return price_selected
+
+
+def persist_ebs_list(ebs_titles):
     payload = json.dumps([ob.__dict__ for ob in ebs_titles])
     url = 'http://localhost:11200/ebsData/saveList'
     headers = {'content-type': 'application/json'}
     post = requests.post(url, data=payload, headers=headers)
     print(post.status_code)
+
+
+def save_ebs_list_file(ebs_titles, ebs_filename, ebs_model, ebs_mode):
+    with open(location + ebs_filename.replace(".csv", "_") + ebs_mode + "_out.csv", 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=';',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(['ISBN', 'Title', 'Subject area', 'price', 'year', 'total usage', 'price per usage', 'selected', 'EBS model ID', 'weighting factor'])
+        for item in ebs_titles:
+            spamwriter.writerow([item.isbn, '"' + item.title + '"', item.subject_area, str(item.price), str(item.year), str(item.total_usage), str(item.cost_per_usage), str(item.selected), ebs_model, str(item.weighting_factor)])
+
+
+def set_bools_usage_for_cost_limit(ebs_titles, cost_limit):
+    total_sum = 0
+    ebs_titles.sort(key=lambda x: x.total_usage, reverse=True)
+    for title in ebs_titles:
+        total_sum += title.price
+        title.selection_usage = (total_sum < cost_limit)
+
+
+def set_bools_cost_per_usage_for_cost_limit(ebs_titles, cost_limit):
+    total_sum = 0
+    ebs_titles.sort(key=lambda x: x.cost_per_usage, reverse=False)
+    for title in ebs_titles:
+        total_sum += title.price
+        title.selection_cost_per_usage = (total_sum < cost_limit)
+
+
+def set_bools_usage_for_usage_limit(ebs_titles, usage_limit):
+    total_sum = 0
+    ebs_titles.sort(key=lambda x: x.total_usage, reverse=True)
+    for title in ebs_titles:
+        total_sum += title.total_usage
+        title.selection_usage = (total_sum < usage_limit)
+
+
+def set_bools_cost_per_usage_for_usage_limit(ebs_titles, usage_limit):
+    total_sum = 0
+    ebs_titles.sort(key=lambda x: x.cost_per_usage, reverse=False)
+    for title in ebs_titles:
+        total_sum += title.total_usage
+        title.selection_cost_per_usage = (total_sum < usage_limit)
+
+
+def set_position_for_usage(ebs_titles):
+    ebs_titles.sort(key=lambda x: x.total_usage, reverse=True)
+    for idx, val in enumerate(ebs_titles):
+        val.weighting_factor += idx
+
+
+def set_position_for_cost_per_usage(ebs_titles):
+    ebs_titles.sort(key=lambda x: x.cost_per_usage, reverse=False)
+    for idx, val in enumerate(ebs_titles):
+        val.weighting_factor += idx
+
+
+def set_weighting_for_usage(ebs_titles):
+    max_usage = max(title.total_usage for title in ebs_titles)
+    for title in ebs_titles:
+        title.weighting_factor = title.weighting_factor * title.total_usage / max_usage
+
+
+def set_weighting_for_cost_per_usage(ebs_titles):
+    max_cost_per_usage = max(title.cost_per_usage for title in ebs_titles)
+    for title in ebs_titles:
+        title.weighting_factor = title.weighting_factor * float(max_cost_per_usage - title.cost_per_usage) / max_cost_per_usage
+
+
+def set_weighting_for_position_usage(ebs_titles):
+    ebs_titles.sort(key=lambda x: x.total_usage, reverse=False)
+    for idx, val in enumerate(ebs_titles):
+        val.weighting_factor = val.weighting_factor * float(idx - 1) / ebs_titles.__len__()
+
+
+def set_weighting_for_position_cost_per_usage(ebs_titles):
+    ebs_titles.sort(key=lambda x: x.cost_per_usage, reverse=True)
+    for idx, val in enumerate(ebs_titles):
+        val.weighting_factor = val.weighting_factor * float(idx - 1) / ebs_titles.__len__()
+
+
+def set_exponential_weighting_for_usage(ebs_titles):
+    mean_usage = mean(title.total_usage for title in ebs_titles)
+    for title in ebs_titles:
+        title.weighting_factor = title.weighting_factor * math.exp(float(title.total_usage-mean_usage)/mean_usage)
+
+
+def set_exponential_weighting_for_cost_per_usage(ebs_titles):
+    mean_cost_per_usage = mean(title.cost_per_usage for title in ebs_titles)
+    for title in ebs_titles:
+        title.weighting_factor = title.weighting_factor * math.exp(-title.cost_per_usage/mean_cost_per_usage)
+
+
+def set_exponential_weighting_for_position_usage(ebs_titles):
+    ebs_titles.sort(key=lambda x: x.total_usage, reverse=True)
+    for idx, val in enumerate(ebs_titles):
+        val.weighting_factor = val.weighting_factor * math.exp(-float(idx - 1) / ebs_titles.__len__())
+
+
+def set_exponential_weighting_for_position_cost_per_usage(ebs_titles):
+    ebs_titles.sort(key=lambda x: x.cost_per_usage, reverse=False)
+    for idx, val in enumerate(ebs_titles):
+        val.weighting_factor = val.weighting_factor * math.exp(-float(idx - 1) / ebs_titles.__len__())
+
+
+def make_selection_for_usage_with_threshold(ebs_titles, threshold):
+    total_sum = 0
+    ebs_titles.sort(key=lambda x: x.total_usage, reverse=True)
+    for idx, val in enumerate(ebs_titles):
+        if idx < threshold:
+            val.selection_usage = True
+            total_sum += val.price
+        else:
+            val.selection_usage = False
+    return total_sum
+
+
+def make_selection_for_cost_per_usage_with_threshold(ebs_titles, threshold):
+    total_sum = 0
+    ebs_titles.sort(key=lambda x: x.cost_per_usage, reverse=False)
+    for idx, val in enumerate(ebs_titles):
+        if idx < threshold:
+            val.selection_cost_per_usage = True
+            total_sum += val.price
+        else:
+            val.selection = False
+    return total_sum
+
+
+def get_price_for_selection(ebs_titles):
+    total_sum = 0
+    for title in ebs_titles:
+        title.selected = title.selection_cost_per_usage and title.selection_usage
+        if title.selected:
+            total_sum += title.price
+    return total_sum
+
+
+def get_price_for_list_with_weighting(ebs_titles, limit):
+    total_sum = 0
+    ebs_titles.sort(key=lambda x: x.weighting_factor, reverse=True)
+    for title in ebs_titles:
+        if total_sum < limit:
+            total_sum += title.price
+            title.selected = True
+        else:
+            title.selected = False
+    return total_sum
+
+
+def get_price_for_list_with_factor(ebs_titles, limit):
+    total_sum = 0
+    ebs_titles.sort(key=lambda x: x.weighting_factor, reverse=False)
+    for title in ebs_titles:
+        if total_sum < limit:
+            total_sum += title.price
+            title.selected = True
+        else:
+            title.selected = False
+    return total_sum
